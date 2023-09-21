e32eae254c06da9ec0ae3e32b1edb3594190bb2b
david <david.montejano@hotmail.com>
2023-09-21 19:06:54 +0200

    Prueba

--- app/src/main/java/com/example/aprendiendoenandroid/PrimeraApp/PrimerAppActivity.kt
+++ app/src/main/java/com/example/aprendiendoenandroid/PrimeraApp/PrimerAppActivity.kt
@@ -1,13 +1,34 @@
 package com.example.aprendiendoenandroid.PrimeraApp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.example.aprendiendoenandroid.R
 
 class PrimerAppActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primer_app)
+        val botonEnviar = findViewById<AppCompatButton>(R.id.BotonEnviar)
+        val edirText = findViewById<AppCompatEditText>(R.id.EditText)
+        val editTextpass = findViewById<AppCompatEditText>(R.id.EditTextPassware)
+
+
+        botonEnviar.setOnClickListener {
+            val usuario = edirText.text.toString()
+            val passware = editTextpass.text.toString()
+
+            if(usuario.isNotEmpty()) {
+
+                Log.i("David", "Hola: $usuario con contraseña $passware ")
+            }else{
+                Log.i("david","Usuario vacio")
+            }
+        }
 
         //Inicio al arrancar la pantalla en plan main
     }
